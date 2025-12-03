6cca85affa0c71ed4632dfadc79989e02447cf26
Sebastian Martinez <sema5085@colorado.edu>
2025-12-03 13:22:25 -0700

    new postgres url

--- ProjectSourceCode/init_db.sh
+++ ProjectSourceCode/init_db.sh
@@ -4,10 +4,9 @@
 # This file contains sensitive information and should be kept private
 
 # TODO: Set your PostgreSQL URI - Use the External Database URL from the Render dashboard
-PG_URI="postgresql://users_db_tia4_user:yg8debGN4TUz7W8v6LYU0Aqa0hlu3Q5h@dpg-d4o9bpali9vc73cb7b60-a.oregon-postgres.render.com/users_db_tia4"
-
-# Execute each .sql file in the directory
+PG_URI=${DATABASE_URL}
+echo ${DATABASE_URL}
 for file in init_data/*.sql; do
     echo "Executing $file..."
-    psql $PG_URI -f "$file"
+    psql "$PG_URI" -f "$file"
 done
